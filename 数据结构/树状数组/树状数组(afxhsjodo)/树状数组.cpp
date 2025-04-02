0c0f4311706e35c0ba246698df203cc50c857190
afxhsjodo <167117162+afxhsjodo@users.noreply.github.com>
2025-04-02 09:17:04 +0800

    Update 树状数组.cpp

--- 数据结构/树状数组/树状数组(afxhsjodo)/树状数组.cpp
+++ 数据结构/树状数组/树状数组(afxhsjodo)/树状数组.cpp
@@ -15,58 +15,22 @@
 using namespace std;
 #define lowbit(x) (x&(-x))
 const int N = 32005;
-int tr[N];
-
-void add(int x, int c)
+struct Fenwick
 {
-    for (int i = x+1; i <= N; i += lowbit(i))tr[i] += c;//注意是i<=N,不是i<=n，x+1是为了防止x=0的情况，x=0时会死循环
-}
-int sum(int x)
-{
-    int res = 0;
-    for (int i = x+1; i; i -= lowbit(i))res += tr[i];
-    return res;
-}
-
-void add1(int x, int c)//树状数组求LIS,两个函数中都只需把+=改成max
-{
-    for (int i = x+1; i <= N; i += lowbit(i))tr[i] = max(tr[i], c);  
-}
-int sum1(int x)//树状数组求LIS
-{
-    int res = 0;
-    for (int i = x+1; i; i -= lowbit(i))res = max(res, tr[i]);
-    return res;
-}
-/*
-树状LIS的main函数内：
-/for (int i = 1; i <= n; i++)
-{
-    f[i] = sum(a[i]) + 1;//单调降写法：a[i]->N-a[i]+1,下面的add也一样
-    add(a[i], f[i]);
-}
-*/
-
-
-int d[N];//求区间最大值
-void add(int x, int c)
-{
-    d[x+1] = c;
-    for (int i = x+1; i <= N; i += lowbit(i))tr[i] = max(tr[i], c);
-}
-int query(int x, int y)
-{
-    int res = 0;
-    x++,y++;
-    while (x <= y)
+    int n;
+    vector<int>tr;
+    Fenwick(int n = 0) { init(n); }
+    void init(int n) { this->n = n; tr.assign(n + 1, 0); }
+    void add(int x, int c)
     {
-        while (y - x >= lowbit(y))
-        {
-            res = max(res, tr[y]);
-            y -= lowbit(y);
-        }
-        res = max(res, d[y]);
-        y--;
+        for (int i = x; i <= n; i += lowbit(i))tr[i] += c;     
     }
-    return res;
-}
+    int sum(int x)
+    {
+        if (!x)return 0;
+        int res = 0;
+        for (int i = x; i; i -= lowbit(i))res += tr[i];
+        return res;
+    }
+};
+
